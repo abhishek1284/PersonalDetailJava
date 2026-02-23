54699ca847a33c1ce302f1ba32e382bdba1a2249
Abhishek Pradhan <abhishek1018insights@gmail.com>
2026-02-23 21:56:56 +0545

    Initial commit: Add PersonalDetails.java

---
+++ src/main/java/PersonalDetail.java
@@ -0,0 +1,9 @@
+public class PersonalDetail {
+    public static void main(String[] args) {
+        System.out.println("Name: Abhishek Pradhan");
+        System.out.println("Address: Kathmandu, Nepal");
+        System.out.println("Age: 24");
+        System.out.println("Phone: 98XXXXXXXX");
+        System.out.println("Email: abhishek@example.com");
+    }
+}
